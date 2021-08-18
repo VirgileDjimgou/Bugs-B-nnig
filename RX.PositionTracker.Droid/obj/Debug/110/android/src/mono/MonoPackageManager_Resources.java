164c06c8bef07027b1fb71120a89d71460e535e6
chichikolon <djimgoupat@yahoo.fr>
2021-08-18 15:29:08 +0200

    first release bugs bönnig

---
+++ RX.PositionTracker.Droid/obj/Debug/110/android/src/mono/MonoPackageManager_Resources.java
@@ -0,0 +1,29 @@
+package mono;
+public class MonoPackageManager_Resources {
+	public static String[] Assemblies = new String[]{
+		/* We need to ensure that "RX.PositionTracker.Droid.dll" comes first in this list. */
+		"RX.PositionTracker.Droid.dll",
+		"Xamarin.Android.Arch.Core.Common.dll",
+		"Xamarin.Android.Arch.Lifecycle.Common.dll",
+		"Xamarin.Android.Arch.Lifecycle.Runtime.dll",
+		"Xamarin.Android.Support.Animated.Vector.Drawable.dll",
+		"Xamarin.Android.Support.Annotations.dll",
+		"Xamarin.Android.Support.Compat.dll",
+		"Xamarin.Android.Support.Core.UI.dll",
+		"Xamarin.Android.Support.Core.Utils.dll",
+		"Xamarin.Android.Support.Design.dll",
+		"Xamarin.Android.Support.Fragment.dll",
+		"Xamarin.Android.Support.Media.Compat.dll",
+		"Xamarin.Android.Support.Transition.dll",
+		"Xamarin.Android.Support.v7.AppCompat.dll",
+		"Xamarin.Android.Support.v7.RecyclerView.dll",
+		"Xamarin.Android.Support.Vector.Drawable.dll",
+		"Xamarin.GooglePlayServices.Base.dll",
+		"Xamarin.GooglePlayServices.Basement.dll",
+		"Xamarin.GooglePlayServices.Location.dll",
+		"Xamarin.GooglePlayServices.Maps.dll",
+		"Xamarin.GooglePlayServices.Tasks.dll",
+	};
+	public static String[] Dependencies = new String[]{
+	};
+}
---
+++ RX.PositionTracker.Droid/obj/Debug/110/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("RX.PositionTracker.Droid.AppWrapper, RX.PositionTracker.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc648b1c8cf8890e0d70.AppWrapper.class, crc648b1c8cf8890e0d70.AppWrapper.__md_methods);
+		
+	}
+}
